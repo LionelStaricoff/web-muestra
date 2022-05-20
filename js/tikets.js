4e247d1a4545f02ddbbd68641b76d00efacd657f
Lionel <staricofflionel@gmail.com>
2022-05-20 01:09:55 -0300

    subiendo web con pagina ticket adicional y funcion de js no funcional

---
+++ js/tikets.js
@@ -0,0 +1,62 @@
+var valor=200;
+function pagos(){
+
+var cantidad= parseInt(document.getElementById("cantidad").innerHTML );
+var categoria= document.getElementById("categoria").innerHTML;
+var descuento;
+
+ 
+switch (categoria) {
+
+    case "Estudiante":
+    descuento = 80;
+    break;
+
+    case "trainee":
+    descuento = 50;
+    break;
+
+    case "Junior":
+    descuento = 15;
+    break;
+}
+
+var imprimeTotal =  cantidad *(valor - (valor * descuento /100) );
+
+var total = document.getElementById("total");
+total.innerHTML += imprimeTotal;
+}
+
+/*
+function botonPagos(){
+    var valor=200;
+    var cantidad= parseInt(document.querySelector("#cantidad").innerHTML );
+    var categoria= document.querySelector("#categoria").innerHTML;
+    var descuento=0;
+    
+     
+    
+    switch (categoria) {
+    
+        case "Estudiante":
+        descuento = 80;
+        break;
+    
+        case "trainee":
+        descuento = 50;
+        break;
+    
+        case "Junior":
+        descuento = 15;
+        break;
+    }
+    
+    
+   var desc = valor*descuento/100;
+   var pagar = (cantidad * (valor - desc));
+
+   var totalAPagar= document.querySelector("#total");
+
+    totalAPagar.innerHTML += pagar;
+    }
+    */
