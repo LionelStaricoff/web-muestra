a01aed977034342d25f811031f83859213a1881a
staricofflionel@gmail.com <staricofflionel@gmail.com>
2022-05-27 02:07:09 -0300

    se agrega diseño web de 2 paginas con js, falta arreglar el first movile del index

--- js/tikets.js
+++ js/tikets.js
@@ -23,41 +23,10 @@ switch (categoria) {
 
 var imprimeTotal =  cantidad *(valor - (valor * descuento /100) );
 
-//var total = document.getElementById("total");
 document.getElementById("total").innerHTML = imprimeTotal;
 
 }
 
-/*
-function botonPagos(){
-    var valor=200;
-    var cantidad= parseInt(document.querySelector("#cantidad").innerHTML );
-    var categoria= document.querySelector("#categoria").innerHTML;
-    var descuento=0;
-    
-     
-    
-    switch (categoria) {
-    
-        case "Estudiante":
-        descuento = 80;
-        break;
-    
-        case "trainee":
-        descuento = 50;
-        break;
-    
-        case "Junior":
-        descuento = 15;
-        break;
-    }
-    
-    
-   var desc = valor*descuento/100;
-   var pagar = (cantidad * (valor - desc));
-
-   var totalAPagar= document.querySelector("#total");
-
-    totalAPagar.innerHTML += pagar;
-    }
-    */
+function borrar(){
+    document.getElementById("total").innerHTML = "";
+}
